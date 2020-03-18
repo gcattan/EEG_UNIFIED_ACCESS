051d68137eebe5d2fc5a59756ee72b28880564fb
gcattan <programgreg@wanadoo.fr>
2020-03-18 22:07:32 +0100

    update sobmodule 2013 and copy classification example in main. Classification with 2012 and 2013 works.

--- main.py
+++ main.py
@@ -28,13 +28,14 @@ import mne
 import warnings
 warnings.filterwarnings("ignore")
 
-exit()
-def classify(dataset):
+def classify2012(dataset):
     scr = {}
     # get the data from subject of interest
     # for subject in dataset.subject_list:
-    for subject in [1,2]:
+    for subject in [1]:
+
         print('running subject', subject)
+
         data = dataset._get_single_subject_data(subject)
         raw = data['session_1']['run_training']
 
@@ -61,7 +62,56 @@ def classify(dataset):
     
     return scr
 
+def classify2013(dataset):
+    scores = {}
+
+    # get the data from subject of interest
+    for subject in [8]:
+        
+        print('running subject', subject)
+
+        scores[subject] = {}
+
+        data = dataset._get_single_subject_data(subject)
+
+        for session in data.keys():			
+
+            print('running session', session)
+
+            raw = data[session]['run_3']
+
+            # filter data and resample
+            fmin = 1
+            fmax = 24
+            raw.filter(fmin, fmax, verbose=False)
+
+            # detect the events and cut the signal into epochs
+            events = mne.find_events(raw=raw, shortest_event=1, verbose=False)
+            event_id = {'NonTarget': 33286, 'Target': 33285}
+            epochs = mne.Epochs(raw, events, event_id, tmin=0.0, tmax=1.0, baseline=None, verbose=False, preload=True)
+            epochs.pick_types(eeg=True)
+
+            # get trials and labels
+            X = epochs.get_data()
+            y = events[:, -1]
+            y[y == 33286] = 0
+            y[y == 33285] = 1
+
+            # cross validation
+            skf = StratifiedKFold(n_splits=5)
+            clf = make_pipeline(ERPCovariances(estimator='lwf', classes=[1]), MDM())
+            scr = cross_val_score(clf, X, y, cv=skf, scoring='roc_auc')
+
+            # print results of classification
+            scores[subject][session] = scr.mean()
+
+    return scores
+
 #load BrainInvaders2012 instance
+
 dataset_2012 = BrainInvaders2012(Training=True)
-scr = classify(dataset_2012)
+scr = classify2012(dataset_2012)
+
+# dataset_2013 = BrainInvaders2013(NonAdaptive=True, Adaptive=False, Training=True, Online=False)
+# scr = classify2013(dataset_2013)
 print(scr)

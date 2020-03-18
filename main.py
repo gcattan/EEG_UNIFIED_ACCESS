3d58ae5582d1d01091ffcbb9c5b5dadf6b384ae1
gcattan <programgreg@wanadoo.fr>
2020-03-18 22:33:15 +0100

    update 2014a and classification test.

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ import mne
 import warnings
 warnings.filterwarnings("ignore")
 
-def baseFilter(raw):
-    raw.filter(1, 24, verbose=False)
+def baseFilter(raw, minF, maxF):
+    raw.filter(minF, maxF, verbose=False)
 
-def epoching(raw, NonTarget, Target):
+def epoching(raw, NonTarget, Target, tmin, tmax):
     events = mne.find_events(raw=raw, shortest_event=1, verbose=False)
     event_id = {'NonTarget': NonTarget, 'Target': Target}
-    epochs = mne.Epochs(raw, events, event_id, tmin=0.0, tmax=1.0, baseline=None, verbose=False, preload=True)
+    epochs = mne.Epochs(raw, events, event_id, tmin=tmin, tmax=tmax, baseline=None, verbose=False, preload=True)
     epochs.pick_types(eeg=True)
     return (events, epochs)
 
@@ -55,10 +55,10 @@ def classify2012(dataset):
         raw = data['session_1']['run_training']
 
         # filter data and resample
-        baseFilter(raw)
+        baseFilter(raw, 1, 24)
 
         # detect the events and cut the signal into epochs
-        events, epochs = epoching(raw, 1, 2)
+        events, epochs = epoching(raw, 1, 2, 0.0, 0.1)
 
         # get trials and labels
         X = epochs.get_data()
@@ -89,10 +89,10 @@ def classify2013(dataset):
             raw = data[session]['run_3']
 
             # filter data and resample
-            baseFilter(raw)
+            baseFilter(raw, 1, 24)
 
             # detect the events and cut the signal into epochs
-            events, epochs = epoching(raw, 33286, 33285)
+            events, epochs = epoching(raw, 33286, 33285, 0.0, 0.1)
 
             # get trials and labels
             X = epochs.get_data()
@@ -108,12 +108,41 @@ def classify2013(dataset):
 
     return scores
 
+def classify2014a(dataset):
+    scr = {}
+    for subject in [1]:
+
+        #load data
+        print('running subject', subject)
+        sessions = dataset._get_single_subject_data(subject)
+        raw = sessions['session_1']['run_1']
+
+        # filter data and resample
+        baseFilter(raw, 1, 20)
+
+        # detect the events and cut the signal into epochs
+        events, epochs = epoching(raw, 1, 2, 0.0, 0.8)
+
+        # get trials and labels
+        X = epochs.get_data()
+        y = epochs.events[:,-1]
+        y = y - 1
+
+        # cross validation
+        scr[subject] = crossValidation(X, y)
+
+    return scr
+
+
 #load BrainInvaders2012 instance
 
 # dataset_2012 = BrainInvaders2012(Training=True)
 # scr = classify2012(dataset_2012)
 
-dataset_2013 = BrainInvaders2013(NonAdaptive=True, Adaptive=False, Training=True, Online=False)
-scr = classify2013(dataset_2013)
+# dataset_2013 = BrainInvaders2013(NonAdaptive=True, Adaptive=False, Training=True, Online=False)
+# scr = classify2013(dataset_2013)
+
+dataset_2014a = BrainInvaders2014a()
+scr = classify2014a(dataset_2014a)
 
 print(scr)

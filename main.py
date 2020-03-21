5d06ec1e393e18f7e69ff63b6e537d5a178d91a9
gcattan <programgreg@wanadoo.fr>
2020-03-21 20:48:35 +0100

    define target/nontarget classes in crossvalidation

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ def epoching(raw, Class1Value, Class2Value, tmin, tmax, Class1Name = 'NonTarget'
     return (events, epochs, event_id)
 
 # cross validation
-def crossValidationERP(X, y, Target = 1):#{target, nontarget, isTarget?}
+def crossValidationERP(X, y, ClassName='Target', ClassInfo={'Target':1, 'NonTarget':2},):
     skf = StratifiedKFold(n_splits=5)
-    clf = make_pipeline(ERPCovariances(estimator='lwf', classes=[Target]), MDM())# TODO Fix classes=[1]
+    clf = make_pipeline(ERPCovariances(estimator='lwf', classes=[ClassInfo[ClassName]]), MDM())# TODO Fix classes=[1]
     return cross_val_score(clf, X, y, cv=skf, scoring='roc_auc').mean()
 
 def crossValidation(X, y):
@@ -58,10 +58,10 @@ def crossValidation(X, y):
     clf = make_pipeline(Covariances(estimator='lwf'), MDM())
     return cross_val_score(clf, X, y, cv=skf).mean()
 
-def crossValidationVR(X_training, labels_training, X_test, labels_test, Target = 1):
+def crossValidationVR(X_training, labels_training, X_test, labels_test, ClassName='Target', ClassInfo={'Target':1, 'NonTarget':0}):
     # estimate the extended ERP covariance matrices with Xdawn
     #dict_labels = {'Target':1, 'NonTarget':0}
-    erpc = ERPCovariances(classes=[Target], estimator='lwf')
+    erpc = ERPCovariances(classes=[ClassInfo[ClassName]], estimator='lwf')
     erpc.fit(X_training, labels_training)
     covs_training = erpc.transform(X_training)
     covs_test = erpc.transform(X_test)
@@ -84,6 +84,11 @@ def classify2012(dataset):
     scr = {}
     # get the data from subject of interest
     # for subject in dataset.subject_list:
+
+    #Target|NonTarget
+    #tmin|tmax
+    #resampling
+    #subject
     for subject in [1]:
 
         print('running subject', subject)
@@ -371,8 +376,8 @@ def classifyPHMDML(dataset):
 # dataset_alphaWaves = AlphaWaves(useMontagePosition=False)
 # scr = classifyAlphaWaves(dataset_alphaWaves)
 
-dataset_VR = VirtualReality(useMontagePosition=False)
-scr = classifyVR(dataset_VR)
+# dataset_VR = VirtualReality(useMontagePosition=False)
+# scr = classifyVR(dataset_VR)
 
 # dataset_PHMDML = HeadMountedDisplay(useMontagePosition=False)
 # scr = classifyPHMDML(dataset_PHMDML)

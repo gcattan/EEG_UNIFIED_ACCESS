1d85b76bd3466e5e2e0ed0c9c3430127b86908fe
gcattan <programgreg@wanadoo.fr>
2020-04-20 23:04:41 +0200

    status error when action incoorrect class for client start server on premise

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from main import run_request
+from classification_wrapper import run_request
+from filelock import FileLock
 
 PORT_NUMBER = 8585
 
@@ -19,28 +20,34 @@ class apiCall(BaseHTTPRequestHandler):
     # Handler for the GET requests
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+
         # Send the html message
         action, params = self._get_action_()
         params = str(params).replace("%20", " ")
         if(action == "request"):
             answer = str(run_request(params))
+            self.send_response(200)
         else:
-            answer = "action not known"
+            self.send_response(400)
+            answer = "Action '" + str(action) + "' is not valid"
+
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
         self.wfile.write(bytes(answer, "utf-8"))
         return
 
 
 try:
-    # Create a web server and define the handler to manage the
-    # incoming request
-    server = HTTPServer(('', PORT_NUMBER), apiCall)
-    print('Started httpserver on port ', PORT_NUMBER)
 
-    # Wait forever for incoming htto requests
-    server.serve_forever()
+    lock = FileLock("server.lock")
+    with lock:
+        # Create a web server and define the handler to manage the
+        # incoming request
+        server = HTTPServer(('', PORT_NUMBER), apiCall)
+        print('Started httpserver on port ', PORT_NUMBER)
+
+        # Wait forever for incoming htto requests
+        server.serve_forever()
 
 except KeyboardInterrupt:
     print('^C received, shutting down the web server')
--- client.py
+++ client.py
@@ -1,14 +1,38 @@
 
-import http.client
 from ast import literal_eval
+import http.client
+import os
+import time
+import subprocess
 
-pload = "@cache get-scores-in bi2012 using subject=[1], tmax=[0.7]"
 
-connection = http.client.HTTPConnection("localhost:8585")
-connection.request("GET", "/request?" + pload.replace(" ", "%20"))
-response = connection.getresponse()
-body = literal_eval(response.readline().decode("utf-8"))
-# print("Status: {} and reason: {}".format(response.status, response.reason))
-print(body)
+def startAndWaitForServer():
+    subprocess.Popen(["python", "api.py"])
+    while not os.path.exists("server.lock"):
+        time.sleep(0.1)
+    return
 
-connection.close()
+
+class ClientRequest():
+
+    def __init__(self, str_request):
+        self.pload = str_request
+        self.isCache = True
+        self.conditions = []
+        startAndWaitForServer()
+
+    def useCache(self, isCache):
+        self.isCache = isCache
+
+    def execute(self):
+        connection = http.client.HTTPConnection("localhost:8585")
+        connection.request("GET", "/request?" + self.pload.replace(" ", "%20"))
+        response = connection.getresponse()
+        connection.close()
+
+        return literal_eval(response.readline().decode("utf-8"))
+
+
+request = ClientRequest(
+    "@cache get-scores-in bi2012 using subject=[1], tmax=[0.7]")
+print(request.execute())

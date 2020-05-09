8342ff7323cbcff6385e55295ff570ab4e4e310d
gcattan <programgreg@wanadoo.fr>
2020-05-09 22:20:19 +0200

    before some refactorings

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from client import ClientRequest, join, autoclean
-from terminal_symbols import PHMD
+from terminal_symbols import ALL
 
 autoclean()
 request = ClientRequest()
 request.useCache(True)
 # request.setKeywords(["subject=1", "pair=5"])
 
-validation_method = """
-ret = 0
-X = args[0]
-y = args[1]
-skf = StratifiedKFold(n_splits=5)
-clf = make_pipeline(Covariances(estimator='lwf'), MDM())
-ret = cross_val_score(clf, X, y, cv=skf).mean()
-"""
+# validation_method = """
+# ret = 0
+# X = args[0]
+# y = args[1]
+# skf = StratifiedKFold(n_splits=5)
+# clf = make_pipeline(Covariances(estimator='lwf'), MDM())
+# ret = cross_val_score(clf, X, y, cv=skf).mean()
+# """
 
-request['validation'] = ({validation_method}, PHMD)
+# request['validation'] = ({validation_method}, PHMD)
+request['subject'] = ('all', ALL)
 # request['tmax'] = ([0.7], BI_2012)
 answer = request.execute()
 print(answer)

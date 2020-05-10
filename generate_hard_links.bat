c7c72745c24e9710c395a8dc3fe782044350cb7a
gcattan <programgreg@wanadoo.fr>
2020-05-10 11:10:15 +0200

    move submodules

--- generate_hard_links.bat
+++ generate_hard_links.bat
@@ -9,13 +9,13 @@ rmdir headmounted
 rmdir alphawaves
 rmdir virtualreality
 rmdir moabb
-mklink /j "braininvaders2012" ".\py.BI.EEG.2012-GIPSA\braininvaders2012"
-mklink /j "braininvaders2013" ".\py.BI.EEG.2013-GIPSA\braininvaders2013"
-mklink /j "braininvaders2014a" ".\py.BI.EEG.2014a-GIPSA\braininvaders2014a"
-mklink /j "braininvaders2014b" ".\py.BI.EEG.2014b-GIPSA\braininvaders2014b"
-mklink /j "braininvaders2015a" ".\py.BI.EEG.2015a-GIPSA\braininvaders2015a"
-mklink /j "braininvaders2015b" ".\py.BI.EEG.2015b-GIPSA\braininvaders2015b"
-mklink /j "headmounted" ".\py.PHMDML.EEG.2017-GIPSA\headmounted"
+mklink /j "braininvaders2012" ".\server\dependencies\py.BI.EEG.2012-GIPSA\braininvaders2012"
+mklink /j "braininvaders2013" ".\server\dependencies\py.BI.EEG.2013-GIPSA\braininvaders2013"
+mklink /j "braininvaders2014a" ".\server\dependencies\py.BI.EEG.2014a-GIPSA\braininvaders2014a"
+mklink /j "braininvaders2014b" ".\server\dependencies\py.BI.EEG.2014b-GIPSA\braininvaders2014b"
+mklink /j "braininvaders2015a" ".\server\dependencies\py.BI.EEG.2015a-GIPSA\braininvaders2015a"
+mklink /j "braininvaders2015b" ".\server\dependencies\py.BI.EEG.2015b-GIPSA\braininvaders2015b"
+mklink /j "headmounted" ".\server\dependencies\py.PHMDML.EEG.2017-GIPSA\headmounted"
 mklink /j "alphawaves" ".\server\dependencies\py.ALPHA.EEG.2017-GIPSA\alphawaves"
-mklink /j "virtualreality" ".\py.VR.EEG.2018-GIPSA\virtualreality"
-mklink /j "moabb" ".\NeuroTechX.moabb\moabb"
+mklink /j "virtualreality" ".\server\dependencies\py.VR.EEG.2018-GIPSA\virtualreality"
+mklink /j "moabb" ".\server\dependencies\NeuroTechX.moabb\moabb"
